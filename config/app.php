e1d78dc8ac8fc702fb8a232accfde777a2bea5ce
Waleed Ahmad <waleedgplus@gmail.com>
2019-06-18 19:13:31 +0500

    Fixed: registered Facade name for Instagram class

--- config/app.php
+++ config/app.php
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'YOUR_ALIAS' => App\Facades\Instagram::class,
+        'Instagram' => App\Facades\Instagram::class,
 
     ],
 
